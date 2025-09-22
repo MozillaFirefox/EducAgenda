c1d1977be96f959a388bbcdc847926080878f6a4
Aaron Nogueira <162352525+MozillaFirefox@users.noreply.github.com>
2025-09-22 11:21:42 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
+import React, { useState } from 'react';
+import { View, TextInput, Text, Button, ScrollView, TouchableOpacity } from 'react-native';
+import styles from './styles'; // Importando os estilos
 
+export default function App() {
+  // Definindo o estado para o nome
+  const [text_nome, setText_nome] = useState('');
+
+  return (
+    <View style={styles.container_principal}>
+      <View>
+        <Text style={styles.texto_instituicao}> EducAgenda </Text>
+      </View>
+      <View>
+        <Text>Email:</Text>
+        <TextInput
+          style={styles.texto_caixa}
+          placeholder="Digite seu email..."
+          placeholderTextColor="gray"
+          value={text_nome}
+          onChangeText={setText_nome}
+        />
+        <Text>Senha:</Text>
+        <TextInput
+          style={styles.texto_caixa}
+          placeholder="Digite sua senha..."
+          placeholderTextColor="gray"
+          value={text_nome}
+          onChangeText={setText_nome}
+        />
+      </View>
+    </View>
+  );
+}
